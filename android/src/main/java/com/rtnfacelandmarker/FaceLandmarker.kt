819b1bdf8424be112bf87073ba0708e0886aa237
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-26 22:07:04 -0400

    delete callback method

--- android/src/main/java/com/rtnfacelandmarker/FaceLandmarker.kt
+++ android/src/main/java/com/rtnfacelandmarker/FaceLandmarker.kt
@@ -4,7 +4,21 @@ import android.content.Context
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.compose.ui.platform.ComposeView
-
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.ExperimentalGetImage
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.LocalContext
+import com.google.mlkit.vision.facemesh.FaceMeshDetection
+import com.google.mlkit.vision.facemesh.FaceMeshDetector
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class FaceLandmarker(context: Context) : LinearLayout(context) {
 
@@ -15,17 +29,73 @@ class FaceLandmarker(context: Context) : LinearLayout(context) {
     private lateinit var defaultDetector: FaceMeshDetector
     private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Log.i("kilo", "Permission granted")
+            shouldShowCamera.value = true
+        } else {
+            Log.i("kilo", "Permission denied")
+        }
+    }
+
     init {
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        defaultDetector = FaceMeshDetection.getClient(
+//            FaceMeshDetectorOptions.Builder()
+//                .setUseCase(FaceMeshDetectorOptions.BOUNDING_BOX_ONLY)
+//                .build()
+        )
+        
         val layoutParams = ViewGroup.LayoutParams(
             ViewGroup.LayoutParams.WRAP_CONTENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
         setLayoutParams(layoutParams)
         orientation = VERTICAL
+
+        // preview = PreviewView(context)
+        preview.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        // addView(preview)
+
         composeView.setContent {
-            FaceLandmarkerText()
+            if (shouldShowCamera.value) {
+                CameraView(
+                    executor = cameraExecutor,
+                    defaultDetector = defaultDetector,
+                    context = LocalContext.current
+                )
+            }
         }
 
         addView(composeView)
     }
+
+    private fun setupLayoutHack() {
+        Choreographer.getInstance().postFrameCallback(object : Choreographer.FrameCallback {
+            override fun doFrame(frameTimeNanos: Long) {
+                manuallyLayoutChildren()
+                viewTreeObserver.dispatchOnGlobalLayout()
+                Choreographer.getInstance().postFrameCallback(this)
+            }
+        })
+    }
+
+    private fun manuallyLayoutChildren() {
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            child.measure(
+                MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY)
+            )
+            child.layout(0, 0, child.measuredWidth, child.measuredHeight)
+        }
+    }
+
 }
--- js/RTNFaceLandmarkerNativeComponent.ts
+++ js/RTNFaceLandmarkerNativeComponent.ts
@@ -1,14 +1,8 @@
 import type {ViewProps} from 'ViewPropTypes';
 import type {HostComponent} from 'react-native';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-import {DirectEventHandler} from 'react-native/Libraries/Types/CodegenTypes';
-
-type Event = ReadOnly<{
-  value: string;
-}>;
 
 export interface NativeProps extends ViewProps {
-  onCameraStart?: DirectEventHandler<Event>;
 }
 
 export default codegenNativeComponent<NativeProps>(
